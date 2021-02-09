b92c9740a2241824bc06bedaa557c73719fd4651
Shellsea <elainesbarrera@gmail.com>
2021-02-08 18:09:32 -0800

    FIXED: workout model

--- models/Workout.js
+++ models/Workout.js
@@ -3,12 +3,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  type: String,
-  name: String,
-  duration: Number,
-  weight: Number,
-  reps: Number,
-  sets: Number,
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: String,
+      name: String,
+      duration: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number,
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
