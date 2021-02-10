dcb5ec23ea530765ff48b70f6dfe9d22b0fcbe3c
Shellsea <elainesbarrera@gmail.com>
2021-02-09 16:43:26 -0800

    FIXED: get route for recent workout, changed api.js json -1

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,10 +4,16 @@ const db = require("../models");
 module.exports = (app) => {
   // get most recent workout
   app.get("/api/workouts", async (req, res) => {
-    const recent = await db.Workout.find({})
-    console.log(recent)
+    const recent = await db.Workout.find({}).sort({ day: -1 });
+    const lastWorkout = {
+      _id: recent[0]._id,
+      day: recent[0].day,
+      totalDuration: recent[0].exercises[0].duration,
+      exercises: recent[0].exercises,
+    };
 
-    res.json(recent)
+    console.log(lastWorkout);
+    res.json(lastWorkout);
   });
   // add an exercise
 
