1b1495f87498bfdf21f43e5c1c08c1cfce52e9e9
Shellsea <elainesbarrera@gmail.com>
2021-02-11 15:10:16 -0800

    helpful notes in api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,6 +4,7 @@ const Workout = require("../models/Workout");
 module.exports = (app) => {
   // get most recent workout
   app.get("/api/workouts", async (req, res) => {
+    // find by descending order and send data from first index
     const recent = await Workout.find({}).sort({ day: -1 });
     const lastWorkout = {
       _id: recent[0]._id,
@@ -16,6 +17,7 @@ module.exports = (app) => {
   });
   // add an exercise
   app.put("/api/workouts/:id", (req, res) => {
+    // important set new to true to save new data
     Workout.findByIdAndUpdate(
       req.params.id,
       { $push: { exercises: req.body } },
@@ -42,6 +44,7 @@ module.exports = (app) => {
   });
   // get ranges for a workout
   app.get("/api/workouts/range", (req, res) => {
+    // find descending order and limit to 7
     Workout.find({})
       .sort({ day: -1 })
       .limit(7)
