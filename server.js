e4b85ad41c9ddcfb6360a19f524a459907fa8aea
Shellsea <elainesbarrera@gmail.com>
2021-02-11 15:08:01 -0800

    helpful comments in server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// require node packages
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+// set PORT or environment variable for deployment
 const PORT = process.env.PORT || 8080;
 
 const app = express();
@@ -14,6 +16,7 @@ app.use(express.json());
 // serve html
 app.use(express.static("public"));
 
+// connect to mongoose database with Atlas or local machine
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
